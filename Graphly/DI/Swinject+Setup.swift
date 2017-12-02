3d540682e7965e1de7e832b3f52a03eb7f07bc86
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2017-12-02 17:12:01 +0100

    Updated pods, changed swift version

---
+++ Graphly/DI/Swinject+Setup.swift
@@ -0,0 +1,28 @@
+//
+// Created by Marcin Karmelita on 12/2/17.
+// Copyright (c) 2017 Marcin Karmelita. All rights reserved.
+//
+
+import Foundation
+//import Swinject
+//import SwinjectStoryboard
+//
+//extension SwinjectStoryboard {
+//    @objc class func setup() {
+//        setupViewControllers()
+//    }
+//
+//    @objc fileprivate class func setupViewControllers() {
+//        defaultContainer.storyboardInitCompleted(GridChartViewController.self) { r, c in
+//
+//        }
+//
+//        defaultContainer.storyboardInitCompleted(GridChartViewController.self) { r, c in
+//
+//        }
+//
+//        defaultContainer.storyboardInitCompleted(GridChartViewController.self) { r, c in
+//
+//        }
+//    }
+//}
--- Podfile
+++ Podfile
@@ -1,16 +1,13 @@
 # Uncomment the next line to define a global platform for your project
-platform :osx, '10.12'
+# platform :osx, '10.12'
 
 target 'Graphly' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Graphly
-	pod 'SwiftCSV'
-  pod 'Charts', '~> 3.0'
-  pod 'SwiftyBeaver'
+  pod 'Charts', :git => 'https://github.com/danielgindi/Charts/', commit: '54171609c2f7d987a46eab60eb0422318d08d266'
   pod 'Swinject', '~> 2.1.1'
-  pod 'SwinjectStoryboard', '~> 1.1.2'
 
 
 
