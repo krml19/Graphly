1411cb8dec6b20fcb22d7a89990a7bf685e02604
Marcin Karmelita <karmelita.marcin.mk@gmail.com>
2017-12-02 11:15:02 +0100

    Init

---
+++ Graphly/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Graphly
+//
+//  Created by Marcin Karmelita on 12/2/17.
+//  Copyright © 2017 Marcin Karmelita. All rights reserved.
+//
+
+import Cocoa
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+    }
+
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+    }
+
+
+
+}
---
+++ Graphly/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Graphly
+//
+//  Created by Marcin Karmelita on 12/2/17.
+//  Copyright © 2017 Marcin Karmelita. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+}
